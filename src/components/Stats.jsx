dc30b2edf34e8c122d75929f294bbdaf5d66971f
Ehiz-js <azamegbesamuel@gmail.com>
2025-05-07 02:37:15 +0100

    initial commit

---
+++ src/components/Stats.jsx
@@ -0,0 +1,28 @@
+export default function Stats({ items }) {
+	if (!items.length)
+		return (
+			<p className="footer">
+				<em>Start adding some items to your packing list 🚀</em>
+			</p>
+		);
+
+	const numItems = items.length;
+	const numPacked = items.filter((item) => item.packed).length;
+	const packedPercent =
+		numItems !== 0 ? Math.round((numPacked / numItems) * 100) : 0;
+
+	return (
+		<footer className="stats">
+			<em>
+				{packedPercent === 100 ? (
+					"You got everything? Ready to go✈️"
+				) : (
+					<>
+						👜You have {numItems} items on your list and you already packed{" "}
+						{numPacked} ({packedPercent}%)
+					</>
+				)}
+			</em>
+		</footer>
+	);
+}
